dd9f4a5bdee062a8a01ccc8a87f9e61d65f23e8b
HyejinDev <wls0602@gmail.com>
2022-12-13 23:23:38 +0900

    12.13 portfolio 2

--- dev/dev/Online Lesson/portfolio.js
+++ dev/dev/Online Lesson/portfolio.js
@@ -95,3 +95,87 @@ document
 document
   .getElementById("forthDot")
   .addEventListener("click", currentImageSlide.bind(null, 4));
+
+/* PORTFOLIO AREA*/
+
+function filterSelection(id) {
+  var X, i;
+
+  X = document.getElementsByClassName("listItem");
+  for (i = 0; i < X.length; i++) {
+    removeClass(X[i], "active");
+  }
+  addClass(document.getElementById(id), "active");
+
+  X = document.getElementsByClassName("filterItem");
+  if (id == "all") id = "";
+  for (i = 0; i < X.length; i++) {
+    removeClass(X[i], "show");
+    if (X[i].className.indexOf(id) > -1) {
+      addClass(X[i], "show");
+    }
+  }
+}
+
+function addClass(element, name) {
+  if (element.className.indexOf(name) == -1) {
+    element.className += " " + name;
+  }
+}
+
+function removeClass(element, name) {
+  var arr;
+  arr = element.className.split(" ");
+
+  while (arr.indexOf(name) > -1) {
+    arr.splice(arr.indexOf(name), 1);
+  }
+
+  element.className = arr.join(" ");
+}
+
+document
+  .getElementById("all")
+  .addEventListener("click", filterSelection.bind(null, "all"));
+document
+  .getElementById("uiux")
+  .addEventListener("click", filterSelection.bind(null, "uiux"));
+document
+  .getElementById("java")
+  .addEventListener("click", filterSelection.bind(null, "java"));
+document
+  .getElementById("db")
+  .addEventListener("click", filterSelection.bind(null, "db"));
+
+function viewportfolio(event) {
+  var polyNode = event.target;
+
+  if (polyNode.tagName.toLowerCase() == "i") {
+    polyNode = polyNode.parentNode;
+  }
+
+  var overlayNode = polyNode;
+  var imageNode = overlayNode.nextElementSibling;
+
+  var itemNode = overlayNode.parentNode;
+  var mainNode = itemNode.nextElementSibling;
+  var subNode = mainNode.nextElementSibling;
+  var textNode = subNode.nextElementSibling;
+
+  document.getElementById("modalImage").src = imageNode.src;
+  document.getElementById("modalMain").innerHTML = mainNode.innerHTML;
+  document.getElementById("modalSub").innerHTML = subNode.innerHTML;
+  document.getElementById("modalText").innerHTML = textNode.innerHTML;
+
+  document.getElementById("portfolioModal").style.display = "block";
+}
+
+document.getElementById("modalClose").addEventListener("click", function () {
+  document.getElementById("portfolioModal").style.display = "none";
+});
+
+var filterItems = document.getElementsByClassName("overlay");
+
+for (var i = 0; i < filterItems.length; i++) {
+  filterItems[i].addEventListener("click", viewportfolio);
+}
